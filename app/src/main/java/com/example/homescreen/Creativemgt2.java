576554e7c7d08bb59259c36e8d883ad097fd5fba
Timvdperk <timvdperk82@gmail.com>
2019-05-14 13:49:52 +0200

    All open day pages have a nav bar now, Note that the route page and all "studies"pages are still missing nav bar

--- app/src/main/java/com/example/homescreen/Creativemgt2.java
+++ app/src/main/java/com/example/homescreen/Creativemgt2.java
@@ -3,8 +3,14 @@ package com.example.homescreen;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,10 +23,55 @@ public class Creativemgt2 extends AppCompatActivity {
     Button share;
     Button question;
     Button route;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creativemgt2);
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if ( id == R.id.myprofile)
+                {
+                    Intent myIntent = new Intent(nav_view.getContext(), HomeScreen.class);
+                    startActivityForResult(myIntent,0);
+                    dl.closeDrawer(GravityCompat.START);
+                }
+                else if ( id == R.id.mysettings)
+                {
+                    Intent myIntent = new Intent(nav_view.getContext(), Studies.class);
+                    startActivityForResult(myIntent,0);
+                    dl.closeDrawer(GravityCompat.START);
+                }
+                else if ( id == R.id.myedit)
+                {
+                    Intent myIntent = new Intent(nav_view.getContext(), InstitutePage.class);
+                    startActivityForResult(myIntent,0);
+                    dl.closeDrawer(GravityCompat.START);
+                }
+                else if ( id == R.id.mysocial)
+                {
+                    Intent myIntent = new Intent(nav_view.getContext(), questionform.class);
+                    startActivityForResult(myIntent,0);
+                    dl.closeDrawer(GravityCompat.START);
+                }
+
+                return true;
+            }
+        });
 
         share = findViewById(R.id.ShareIt);
         share.setOnClickListener(new View.OnClickListener() {
@@ -81,4 +132,11 @@ public class Creativemgt2 extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+
+
+    }
+
 }
--- app/src/main/java/com/example/homescreen/Route.java
+++ app/src/main/java/com/example/homescreen/Route.java
@@ -1,8 +1,15 @@
 package com.example.homescreen;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,11 +21,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Route extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.route_xml);
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -35,4 +45,5 @@ public class Route extends FragmentActivity implements OnMapReadyCallback {
         map.addMarker(new MarkerOptions().position(HRwijnhaven).title("HRwijnhaven"));
         map.moveCamera(CameraUpdateFactory.newLatLng(HRwijnhaven));
     }
+
 }
