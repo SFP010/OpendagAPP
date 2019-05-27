86257695283750ed65a8392ebadb1963a7b82477
TimoPolderman <43845528+TimoPolderman@users.noreply.github.com>
2019-05-27 10:32:04 +0200

    Added contactpage with working buttons

--- app/src/main/java/com/example/homescreen/ContactPage.java
+++ app/src/main/java/com/example/homescreen/ContactPage.java
@@ -1,7 +1,11 @@
 package com.example.homescreen;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactPage extends AppCompatActivity {
 
@@ -9,5 +13,52 @@ public class ContactPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_page);
+
+        Button websiteBtn = findViewById(R.id.website);
+        Button routeBtn = findViewById(R.id.route);
+        Button questionBtn = findViewById(R.id.question);
+
+        routeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRoute();
+            }
+        });
+
+        questionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuestion();
+            }
+        });
+
+        websiteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebsite();
+            }
+
+
+        });
+
+
+
+
     }
+    public void openRoute(){
+        Intent intent = new Intent(this, Route.class);
+        startActivity(intent);
+    }
+
+
+    public void openQuestion(){
+        Intent intent = new Intent(this, questionform.class);
+        startActivity(intent);
+    }
+
+    public void openWebsite(){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hogeschoolrotterdam.nl"));
+        startActivity(intent);
+    }
+
 }
