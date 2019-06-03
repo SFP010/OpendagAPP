9aa600bd960e47deafebca97730c9d2d301bd8f6
Poot <Spoot@hotmail.nl>
2019-06-03 14:01:36 +0200

    Fixed INF openday bug and the coding quiz questions is half-way

--- app/src/main/java/com/example/homescreen/InformaticaOpendag3.java
+++ app/src/main/java/com/example/homescreen/InformaticaOpendag3.java
@@ -28,7 +28,7 @@ public class InformaticaOpendag3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.informatica_opendag2);
+        setContentView(R.layout.informatica_opendag3);
         dl = (DrawerLayout)findViewById(R.id.dl);
         abdt = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
         abdt.setDrawerIndicatorEnabled(true);
